e4618f6b155c63609188a89dc260abb96a5f6e24
phant80m <black.pandazayd@gmail.com>
2026-02-05 11:37:07 +1100

    updated hyprland config

---
+++ .config/fish/conf.d/fish_frozen_theme.fish
@@ -0,0 +1,48 @@
+# This file was created by fish when upgrading to version 4.3, to migrate
+# theme variables from universal to global scope.
+# Don't edit this file, as it will be written by the web-config tool (`fish_config`).
+# To customize your theme, delete this file and see
+#     help interactive#syntax-highlighting
+# or
+#     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
+# for appropriate commands to add to ~/.config/fish/config.fish instead.
+# See also the release notes for fish 4.3.0 (run `help relnotes`).
+
+set --global fish_color_autosuggestion 6e738d
+set --global fish_color_cancel ed8796
+set --global fish_color_command 8aadf4
+set --global fish_color_comment 8087a2
+set --global fish_color_cwd eed49f
+set --global fish_color_cwd_root red
+set --global fish_color_end f5a97f
+set --global fish_color_error ed8796
+set --global fish_color_escape ee99a0
+set --global fish_color_gray 6e738d
+set --global fish_color_history_current --bold
+set --global fish_color_host 8aadf4
+set --global fish_color_host_remote a6da95
+set --global fish_color_keyword ed8796
+set --global fish_color_normal cad3f5
+set --global fish_color_operator f5bde6
+set --global fish_color_option a6da95
+set --global fish_color_param f0c6c6
+set --global fish_color_quote a6da95
+set --global fish_color_redirection f5bde6
+set --global fish_color_search_match --background=363a4f
+set --global fish_color_selection --background=363a4f
+set --global fish_color_status ed8796
+set --global fish_color_user 8bd5ca
+set --global fish_color_valid_path --underline
+set --global fish_pager_color_background
+set --global fish_pager_color_completion cad3f5
+set --global fish_pager_color_description 6e738d
+set --global fish_pager_color_prefix f5bde6
+set --global fish_pager_color_progress 6e738d
+set --global fish_pager_color_secondary_background
+set --global fish_pager_color_secondary_completion
+set --global fish_pager_color_secondary_description
+set --global fish_pager_color_secondary_prefix
+set --global fish_pager_color_selected_background
+set --global fish_pager_color_selected_completion
+set --global fish_pager_color_selected_description
+set --global fish_pager_color_selected_prefix
